ffb620f6afa745e63f460bd4ab3e61209daa8d4d
Nawodya Jayalath <nawodyaonline@gmail.com>
2024-06-23 21:40:02 +0530

    add: Feed locations service - PRODUCER

--- cab-book-driver/src/main/java/com/dailycode/cab_book_driver/controller/CabLocationController.java
+++ cab-book-driver/src/main/java/com/dailycode/cab_book_driver/controller/CabLocationController.java
@@ -22,11 +22,12 @@ public class CabLocationController {
     }
 
     @PutMapping
-    public ResponseEntity updateLocation(){
+    public ResponseEntity updateLocation() throws InterruptedException {
 
         int range = 100;
         while(range > 0){
-            System.out.println(Math.random() + "," + Math.random());
+            cabLocationService.updateLocation(Math.random() + "," + Math.random());
+            Thread.sleep(1000);
             range--;
         }
 
--- cab-book-driver/src/main/java/com/dailycode/cab_book_driver/service/CabLocationService.java
+++ cab-book-driver/src/main/java/com/dailycode/cab_book_driver/service/CabLocationService.java
@@ -1,9 +1,22 @@
 package com.dailycode.cab_book_driver.service;
 
+import com.dailycode.cab_book_driver.constant.AppConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CabLocationService {
 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Autowired
+    public CabLocationService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public boolean updateLocation(String location) {
+        kafkaTemplate.send(AppConstant.CAB_LOCATION, location);
+        return true;
+    }
 }
